e297b15a4cb0ab39d2766a189cb739bf4eac8e7c
Josh <joshpanebianco@protonmail.com>
2023-01-10 00:49:36 +1000

    First commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,19 @@
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities  { get; set; }
+
+    }
+}
